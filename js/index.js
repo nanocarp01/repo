b353f636b6663b5aa865272d27d357774cd6713c
Fernando Heredia <71789047+nanocarp01@users.noreply.github.com>
2021-08-10 03:27:29 -0300

    repo

--- js/index.js
+++ js/index.js
@@ -8,15 +8,119 @@ const config = {
   threshold: [0.6, 0.9],
 };
 
-function change(){
+function ing(){
+  var ini = document.getElementById("inicio");
+  var sobre = document.getElementById("sobre");
+  var cono = document.getElementById("conocimientos");
+  var proy = document.getElementById("proyectos");
+  var cont = document.getElementById("contacto");
   var lang = document.getElementById("lang");
+  var text1 = document.getElementById("home_title--primary");
+  var text2 = document.getElementById("home_title");
+  var text3 = document.getElementById("home_title--secondary");
+  var soy = document.getElementById("quiensoy");
+  var nom = document.getElementById("nombre");
+  var dev = document.getElementById("about-secondary");
+  var soy1 = document.getElementById("soy");
+  var cv = document.getElementById("cv");
+  var skill = document.getElementById("cono");
+  var port = document.getElementById("section_title");
+  var port1 = document.getElementById("section_subtitle");
+  var contact = document.getElementById("contacto1");
+  var bike = document.getElementById("bike");
+  var app = document.getElementById("app");
+  var draw = document.getElementById("draw");
+  var mail = document.getElementById("mail1");
+  var git = document.getElementById("github");
+  var tw  = document.getElementById("tw");
+  var linkedin = document.getElementById("link");
 
-  if(lang.contains == "Ingles"){
-    alert("Ingles");
-    
-  }
+  
+    ini.innerHTML = "Home";
+    sobre.innerHTML = "About";
+    cono.innerHTML = "Skill";
+    proy.innerHTML = "Portfolio";
+    cont.innerHTML = "Contact";
+    text1.innerHTML = "Hello!";
+    text2.innerHTML = "I´m Fernando Heredia";
+    text3.innerHTML = "Developer";
+    soy.innerHTML = "Who am I?";
+    nom.innerHTML = "I´m Fernando Heredia,";
+    dev.innerHTML = "Developer...";
+    soy1.innerHTML = "I am a full stack python developer oriented to the development of websites, and I am constantly learning new development technologies. I love collaborating and working as a team, where i can apply my knowledge and skills to continue improving and growing professionally ";
+    cv.innerHTML = "Dowload CV";
+    cv.href = "./assets/CV_FernandoHeredia_ing.pdf";
+    skill.innerHTML = "My Skills";
+    port.innerHTML = "My Portfolio";
+    port1.innerHTML = "here you can see some of my projects!";
+    contact.innerHTML = "Contact me!";
+    bike.innerHTML = "Website created to complete the frontend stage of the course: HTML, JS, CSS";
+    app.innerHTML = "App for android to know schedules of the groups to the faculty.";
+    draw.innerHTML = "Website Template made for a friend: JS, CSS, HTML";
+    mail.innerHTML = "Mail Me";
+    git.innerHTML = "Follow Me";
+    tw.innerHTML = "Follow Me";
+    linkedin.innerHTML = "Contact Me";
+
+}
+
+function arg(){
+  var ini = document.getElementById("inicio");
+  var sobre = document.getElementById("sobre");
+  var cono = document.getElementById("conocimientos");
+  var proy = document.getElementById("proyectos");
+  var cont = document.getElementById("contacto");
+  var lang = document.getElementById("lang");
+  var text1 = document.getElementById("home_title--primary");
+  var text2 = document.getElementById("home_title");
+  var text3 = document.getElementById("home_title--secondary");
+  var soy = document.getElementById("quiensoy");
+  var nom = document.getElementById("nombre");
+  var dev = document.getElementById("about-secondary");
+  var soy1 = document.getElementById("soy");
+  var cv = document.getElementById("cv");
+  var skill = document.getElementById("cono");
+  var port = document.getElementById("section_title");
+  var port1 = document.getElementById("section_subtitle");
+  var contact = document.getElementById("contacto1");
+  var bike = document.getElementById("bike");
+  var app = document.getElementById("app");
+  var draw = document.getElementById("draw");
+  var mail = document.getElementById("mail1");
+  var git = document.getElementById("github");
+  var tw  = document.getElementById("tw");
+  var linkedin = document.getElementById("link");
+
+
+  ini.innerHTML = "Inicio";
+    sobre.innerHTML = "Sobre Mi";
+    cono.innerHTML = "Mis Habilidades";
+    proy.innerHTML = "Mis Proyectos";
+    cont.innerHTML = "Contacto";
+    text1.innerHTML = "Hola!";
+    text2.innerHTML = "Soy Fernando Heredia";
+    text3.innerHTML = "Desarrollador";
+    soy.innerHTML = "Quien Soy?";
+    nom.innerHTML = "Soy Fernando Heredia,";
+    dev.innerHTML = "Desarrolador...";
+    soy1.innerHTML = "Soy desarrolador fullstack python, orientado al desarrollo de sitios web, y me encuentro en constante aprendizaje de nuevas tecnologias de desarrollo.Me encanta colaborar y trabajar en equipo, donde puedo aplicar mis conocimientos y habilidades para seguir mejorando y creciendo profesionalmente.";
+    cv.innerHTML = "Descargar CV";
+    cv.href = "./assets/CV_FernandoHeredia.pdf";
+    skill.innerHTML = "Mis Habilidades";
+    port.innerHTML = "Mis Proyectos";
+    port1.innerHTML = "Aqui puedes ver algunos de mis proyectos!";
+    contact.innerHTML = "Contacto";
+    bike.innerHTML = "Web realizada para completar etapa frontend de curso: HTML, JS, CSS";
+    app.innerHTML = "App para android para conocer horarios de los colectivos hasta la facultad.";
+    draw.innerHTML = "Template de Pagina realizada para un amigo: JS, CSS, HTML";
+    mail.innerHTML = "Escribeme";
+    git.innerHTML = "Sigueme";
+    tw.innerHTML = "Sigueme";
+    linkedin.innerHTML = "Contactame";
   
 }
+
+
 
 
 function mail(){
